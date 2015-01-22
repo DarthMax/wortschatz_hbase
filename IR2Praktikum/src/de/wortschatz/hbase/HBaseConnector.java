236d15970be19abbf4b50e518beca99f5d8e08fc
DarthMax <max@kopfueber.org>
2015-01-22 14:48:21 +0100

    implement HBaseConnector

--- IR2Praktikum/src/de/wortschatz/hbase/HBaseConnector.java
+++ IR2Praktikum/src/de/wortschatz/hbase/HBaseConnector.java
@@ -1,9 +1,24 @@
 package de.wortschatz.hbase;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
 
 public class HBaseConnector{
 
+    protected static HBaseConnector connection;
+
+    public static Configuration get_connection() {
+        if (HBaseConnector.connection==null) {
+            HBaseConnector.connection = new HBaseConnector();
+        }
+        return HBaseConnector.connection.conf;
+    };
+
+
+    private Configuration conf;
+
+    public HBaseConnector() {
+        this.conf = HBaseConfiguration.create();
+        this.conf.setQuietMode(true);
+    }
 }
